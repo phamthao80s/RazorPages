86c03925228e7af9767dafacb61dddc5c7c119f2
phamthao80s <phamthao80s@gmail.com>
2021-08-25 20:37:54 +0100

    Add a new feild
    Add Rating Property to Models file and all following pages in pages folder. Delete old Data in SSOX and update new Data again in PMC

--- RazorPages/Migrations/20210825193238_Rating.Designer.cs
+++ RazorPages/Migrations/20210825193238_Rating.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorPages.Data;
 
 namespace RazorPages.Migrations
 {
     [DbContext(typeof(RazorPagesContext))]
-    partial class RazorPagesContextModelSnapshot : ModelSnapshot
+    [Migration("20210825193238_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- RazorPages/Models/SeedData.cs
+++ RazorPages/Models/SeedData.cs
@@ -27,6 +27,7 @@ namespace RazorPages.Models
                         Title = "When Harry Met Sally",
                         ReleaseDate = DateTime.Parse("1989-2-12"),
                         Genre = "Romantic Comedy",
+                        Rating = "R",
                         Price = 7.99M
                     },
 
@@ -35,6 +36,7 @@ namespace RazorPages.Models
                         Title = "Ghostbusters ",
                         ReleaseDate = DateTime.Parse("1984-3-13"),
                         Genre = "Comedy",
+                        Rating = "R",
                         Price = 8.99M
                     },
 
@@ -43,6 +45,7 @@ namespace RazorPages.Models
                         Title = "Ghostbusters 2",
                         ReleaseDate = DateTime.Parse("1986-2-23"),
                         Genre = "Comedy",
+                        Rating = "R",
                         Price = 9.99M
                     },
 
@@ -51,6 +54,7 @@ namespace RazorPages.Models
                         Title = "Rio Bravo",
                         ReleaseDate = DateTime.Parse("1959-4-15"),
                         Genre = "Western",
+                        Rating = "R",
                         Price = 3.99M
                     }
                 );
